90ec8cfbf1916916c771cf3ba299326bc0c4672d
Joy <827270377@qq.com>
2018-10-15 15:41:46 +0800

    新建用户

---
+++ src/components/Alert.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="dialog-box" v-if="showAlert" @click="close">
+    <div class="dialog">
+      <h1 class="title">{{title}}</h1>
+      <div class="content">
+        <slot name="main"></slot>
+      </div>
+      <div class="btn-box" :style="{'padding':padding}">
+        <slot name="footer"></slot>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+	props: ["showAlert", "title", "padding"],
+	data() {
+		return {};
+	},
+	methods: {
+		close(e) {
+			if ($(e.target).hasClass("dialog-box")) {
+				this.$emit("closeAlert");
+			}
+		}
+	}
+};
+</script>
+<style lang="stylus" scoped>
+.dialog-box {
+  position: fixed;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  background-color: rgba(0, 0, 0, 0.6);
+  z-index: 9;
+  text-align: center;
+
+  &::after {
+    content: '';
+    display: inline-block;
+    height: 100%;
+    width: 0;
+    vertical-align: middle;
+  }
+}
+
+.dialog {
+  display: inline-block;
+  vertical-align: middle;
+  min-width: 400px;
+  border-radius: 5px;
+  background-color: #fff;
+  border: 1px solid #ccc;
+  text-align: left;
+
+  .title {
+    text-align: center;
+    font-size: 16px;
+    color: #555;
+    background-color: #F2F2F2;
+    line-height: 50px;
+    font-weight: 400;
+    border-top-left-radius: 5px;
+    border-top-right-radius: 5px;
+    border-bottom: 1px solid transparent;
+  }
+
+  .content {
+    padding: 20px;
+    max-height: 500px;
+    overflow-y: auto;
+  }
+
+  .btn-box {
+    text-align: right;
+    padding: 10px 10px 20px;
+  }
+}
+</style>
--- src/view/UserListView.vue
+++ src/view/UserListView.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <el-button class="add" type="primary">新增</el-button>
+    <el-button class="add" type="primary" @click="newUser">新增</el-button>
     <el-table :data="list" stripe border :height="tableHeight" style="width: 100%">
       <el-table-column type="index" width="50">
       </el-table-column>
@@ -15,14 +15,66 @@
         </template>
       </el-table-column>
     </el-table>
+    <alert :showAlert="show" @closeAlert="show = false" title="新建用户">
+      <div slot="main">
+        <!-- 消除弹出框像素模糊bug -->
+        <div style="width: 500px;">
+          <el-form ref="form" :rules="rules" :model="form" :label-position="labelPosition" label-width="80px">
+            <el-form-item label="用户名" prop="name">
+              <el-input v-model="form.name"></el-input>
+            </el-form-item>
+            <el-form-item label="密码" prop="pass">
+              <el-input v-model="form.pass" autocomplete="off"></el-input>
+            </el-form-item>
+          </el-form>
+        </div>
+        <div slot="footer" style="text-align:center;">
+          <el-button type="primary" @click="submitForm('form')">立即创建</el-button>
+          <el-button @click="resetForm('form')">重置</el-button>
+        </div>
+      </div>
+    </alert>
   </div>
 </template>
 
 <script>
+import Alert from "@/components/Alert.vue";
+
 export default {
 	name: "MainView",
+	components: {
+		Alert
+	},
 	data() {
+		var validatePass = (rule, value, callback) => {
+			if (value === "") {
+				callback(new Error("请输入密码"));
+			} else {
+				var patrn = /^[a-zA-Z0-9]{6,12}$/;
+				if (!patrn.exec(this.form.pass)) {
+					callback(new Error("密码格式不正确(6-12位字母和数字)"));
+				} else {
+					callback();
+				}
+			}
+		};
 		return {
+			show: false,
+			labelPosition: "left",
+			form: {
+				name: "",
+				pass: ""
+			},
+			rules: {
+				name: [
+					{ required: true, message: "请输入用户名", trigger: "blur" },
+					{ min: 3, max: 5, message: "长度在 3 到 5 个字符", trigger: "blur" }
+				],
+				pass: [
+					{ required: true, message: "请输入密码", trigger: "blur" },
+					{ validator: validatePass, trigger: "blur" }
+				]
+			},
 			tableHeight: window.innerHeight - 125,
 			list: [
 				{ name: "用户1", pass: "111" },
@@ -47,22 +99,6 @@ export default {
 		};
 	},
 	methods: {
-		openDeleteUserBox(index, rows) {
-			this.$confirm("即将删除该用户, 是否继续?", "提示", {
-				confirmButtonText: "确定",
-				cancelButtonText: "取消",
-				type: "warning"
-			})
-				.then(() => {
-					this.deleteUser(index, rows);
-				})
-				.catch(() => {
-					this.$message({
-						type: "info",
-						message: "已取消删除"
-					});
-				});
-		},
 		openEditPsdBox(row) {
 			this.$prompt("请输入密码", "提示", {
 				confirmButtonText: "确定",
@@ -80,7 +116,30 @@ export default {
 					});
 				});
 		},
-
+		// 更改密码
+		changePassword(newpsd, row) {
+			this.$message({
+				type: "success",
+				showClose: true,
+				message: "该用户的新密码是: " + newpsd
+			});
+		},
+		openDeleteUserBox(index, rows) {
+			this.$confirm("即将删除该用户, 是否继续?", "提示", {
+				confirmButtonText: "确定",
+				cancelButtonText: "取消",
+				type: "warning"
+			})
+				.then(() => {
+					this.deleteUser(index, rows);
+				})
+				.catch(() => {
+					this.$message({
+						type: "info",
+						message: "已取消删除"
+					});
+				});
+		},
 		// 删除用户
 		deleteUser(index, rows) {
 			rows.splice(index, 1);
@@ -90,12 +149,21 @@ export default {
 				type: "success"
 			});
 		},
-		// 更改密码
-		changePassword(newpsd, row) {
-			this.$message({
-				type: "success",
-				message: "该用户的新密码是: " + newpsd
+		newUser() {
+			this.show = true;
+		},
+		submitForm(formName) {
+			this.$refs[formName].validate(valid => {
+				if (valid) {
+					console.log('新建用户：' + this.form.name, this.form.pass);
+				} else {
+					console.log("error submit!!");
+					return false;
+				}
 			});
+		},
+		resetForm(formName) {
+			this.$refs[formName].resetFields();
 		}
 	},
 	mounted() {
